995e4310f5411059ee5299912c92eb92a99a4dd9
V0idCoder <v0idcoder@proton.me>
2023-11-24 08:50:55 +0100

    Beta Login Page

--- routes/User.js
+++ routes/User.js
@@ -31,4 +31,35 @@ try{
     res.status(500).send("Une erreur est survenue")
 }
 });
+
+router.get('/login', async (req, res) => {
+try{
+
+    const user = {
+        email: "perhaita@test.com",
+        password: "mdp",
+        firstName: "paul",
+        lastName: "perhaita",
+    };
+
+    let users;
+    try{
+        const userData = await fs.readFile('data/users.json', 'utf-8');
+        users = JSON.parse(userData);
+    }catch (error){
+        users = []
+    }
+
+    users.push(user);
+    await fs.writeFile('data/users.json', JSON.stringify(users, null, 2), 'utf-8')
+
+    
+    res.render('user/signup', { user });
+}catch(error){
+    console.error(error);
+    res.status(500).send("Une erreur est survenue")
+}
+});
+
 export default router;
+
